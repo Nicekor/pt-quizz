06a26fd745f4bdfe6077cdf6bb5b96d5822693e3
Nicekor <zemorais16@hotmail.com>
2020-04-19 22:11:18 +0100

    Added Spinner component

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './spinner.module.css';
+
+const Spinner = () => {
+  return <div className={classes.loader}>Loading...</div>;
+};
+
+export default Spinner;
