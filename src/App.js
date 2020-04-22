62ec4cfda0d253f341ccb314bf8352d35f54bea4
Gustavo Parreira <gustavo.parreira@mypthub.net>
2020-04-23 00:23:22 +0100

    Fixed mobile layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from 'react'
 
-import Header from './components/Header/Header';
-import GameMenu from './components/GameMenu/GameMenu';
+import Header from './components/Header/Header'
+import GameMenu from './components/GameMenu/GameMenu'
 
 function App() {
-  return (
-    <>
-      <Header />
-      <GameMenu />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <GameMenu />
+        </>
+    )
 }
 
-export default App;
+export default App
