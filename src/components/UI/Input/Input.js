feb568f5a4d8aaa9e91e3e6d681d765c4f80c345
Nicekor <zemorais16@hotmail.com>
2020-04-23 21:21:12 +0100

    Added different styling for when the container is focused

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import classes from './input.module.css';
 
 const Input = ({ id, labelName, Component = 'input', icon, ...props }) => {
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
     <div className={classes.wrapper}>
       <label htmlFor={id}>{labelName}</label>
-      <div className={classes.inputContainer}>
-        <Component id={id} {...props} className={classes.input} />
+      <div
+        className={classes.inputContainer}
+        style={isFocused ? { borderBottomColor: '#dcdcdc' } : null}
+      >
+        <Component
+          id={id}
+          {...props}
+          className={classes.input}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+        />
         {icon && (
           <div className={classes.iconContainer}>
             <FontAwesomeIcon icon={icon} />
