05d6cfb7d62d147a07d0fde1f05d7d68b752ec3c
Nicekor <zemorais16@hotmail.com>
2020-04-25 00:59:26 +0100

    Added anchor tag so the page refreshes when the logo is clicked

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,9 @@ import classes from './header.module.css';
 const Header = () => {
   return (
     <div className={classes.headerContainer}>
-      <img src={logo} alt="Megaphone logo" className={classes.logo}></img>
+      <a href="/">
+        <img src={logo} alt="Megaphone logo" className={classes.logo}></img>
+      </a>
       <h1>Quizz Time!</h1>
     </div>
   );
