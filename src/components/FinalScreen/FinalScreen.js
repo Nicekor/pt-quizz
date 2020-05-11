9e64e0223e703a589066f81d2f85c8d13345bafd
Nicekor <zemorais16@hotmail.com>
2020-05-11 15:10:08 +0100

    Finished Final Screen component

--- src/components/FinalScreen/FinalScreen.js
+++ src/components/FinalScreen/FinalScreen.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Button from '../UI/Button/Button';
 
 import classes from './finalScreen.module.css';
+import { LanguageContext } from '../../context/LanguageContext';
+import uiLabel from '../../assets/uiLabel.json';
 
-const FinalScreen = ({ score }) => {
+const FinalScreen = ({ score, onPlayAgain, numQuestions }) => {
+  const { language } = useContext(LanguageContext);
+
   return (
     <div className={[classes.finalScreen, classes.fadeIn].join(' ')}>
       <h1 className={classes.gameOver}>GAME OVER</h1>
-      <p>Final Score: {score}</p>
-      <Button>Play Again</Button>
+      <p>
+        {uiLabel.finalScore[language]}:{' '}
+        <span style={{ color: score >= numQuestions / 2 ? 'green' : 'red' }}>
+          {score}/{numQuestions}
+        </span>
+      </p>
+      <Button onClick={onPlayAgain}>{uiLabel.playAgain[language]}</Button>
     </div>
   );
 };
